1f003fd6c737e128d123a17b00101a625ba25e1a
Wolf007 <99535270+EmdadulHqIram013@users.noreply.github.com>
2024-05-09 19:36:20 +0600

    setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="python_to_bash_tool",
+    name="pytobash",
     version="0.1",
     packages=find_packages(),
     entry_points={
         'console_scripts': [
-            'python-to-bash = python_to_bash.cli:main',
+            'pytobash = python_to_bash.cli:main',
         ],
     },
     install_requires=[
